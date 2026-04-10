9ed528e78ea10c286cbccf863cd991d0d524ee4f
Mattia Salvetti <mattiasalvetti@0735c19c-9c15-47b4-ad45-474edd8de6f4.fritz.box>
2026-04-10 16:19:53 +0200

    First commit

---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew}:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = with pkgs;
+        [ neovim
+	  google-chrome
+	  obsidian
+	  raycast
+	  git
+	  claude-code
+	  ollama
+	  gh
+        ];
+
+	homebrew = {
+	  enable = true;
+	  casks = [];
+	};
+
+	fonts.packages = with pkgs; [
+	  nerd-fonts.jetbrains-mono
+	];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+      system.primaryUser = "mattiasalvetti";
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.config.allowUnfree = true;
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#Mattias-MacBook-Air-2
+    darwinConfigurations."Mattias-MacBook-Air-2" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration 
+      	nix-homebrew.darwinModules.nix-homebrew
+	  {
+	     nix-homebrew = 
+	       {
+	         enable = true;
+	         enableRosetta = true;
+	         user = "mattiasalvetti";
+	       };
+	  }
+	];
+    };
+  };
+}
